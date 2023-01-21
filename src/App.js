26389b75c8ce1a4eb19e3d56ded8419468d67193
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-21 22:46:34 +0530

    spread understood with vid 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounterTwo from './components/HookCounterTwo';
+import HooksThree from './components/HooksThree';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter/> */}
-      <HookCounterTwo/>
+      {/* <HookCounterTwo/> */}
+      <HooksThree />
     </div>
   );
 }
