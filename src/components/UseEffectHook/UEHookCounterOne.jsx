cc357e209ddffc99e2d3d97648e804e15606587e
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-25 07:52:47 +0530

    Vid 8n completed, learned about dependency

--- src/components/UseEffectHook/UEHookCounterOne.jsx
+++ src/components/UseEffectHook/UEHookCounterOne.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react';
 
 const UEHookCounterOne = () => {
     const [counter, setCounter] = useState(0)
+    const [name,setName] = useState('');
+
     useEffect(()=>{
+        console.log('updated')
         document.title = `You clicked ${counter} times`
-    })
+    },[counter])
   return (
     <div>
+    <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
         <button onClick={()=>setCounter(counter+1)}>Click {counter}</button>
     </div>
   )
